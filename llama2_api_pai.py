dd84ba86745b21c4634b646053ba23cc82ce28f4
davidchen <daviddhc20120601@gmail.com>
2023-08-07 17:01:49 +0800

    trying remote llama2

--- llama2_api_pai.py
+++ llama2_api_pai.py
@@ -10,44 +10,44 @@ class pai_llama:
         self.temperature = 0.0
         self.round = 1
         self.received_message = None
-        host = "ws://1694051720363540.cn-hangzhou.pai-eas.aliyuncs.com/api/predict/api_chatllm_llama2_13b"
+        host = "ws://5568953365102451.ap-southeast-1.pai-eas.aliyuncs.com/api/predict/llama_api"
         self.host = host
-    
+
     def on_message(self, ws, message):
-        assert self.input_message in message, "request not in response." 
+        assert self.input_message in message, "request not in response."
         self.received_message = message
-    
+
     def on_error(self, ws, error):
         print('error happened .. ')
         print(str(error))
-    
+
     def on_close(self, ws, a, b):
         print("### closed ###", a, b)
-    
+
     def on_pong(self, ws, pong):
         print('pong:', pong)
-    
+
     def on_open(self, ws):
         print('Opening WebSocket connection to the server ... ')
         params_dict = {}
         params_dict['input_ids'] = self.input_message
         params_dict['temperature'] = self.temperature
         params_dict['repetition_penalty'] = self.repetition_penalty
-        params_dict['top_p'] = self.top_p 
+        params_dict['top_p'] = self.top_p
         params_dict['max_length'] = 2048
         params_dict['num_beams'] = 1
         raw_req = json.dumps(params_dict)
         ws.send(raw_req)
-        
+
         # for i in range(self.round):
-            # ws.send(self.input_message)
-        
+        # ws.send(self.input_message)
+
         ws.send(struct.pack('!H', 1000), websocket.ABNF.OPCODE_CLOSE)
-    
+
     def generate(self, message, temperature=0.95, repetition_penalty=1, top_p=0.01, max_length=2048, num_beams=1):
         self.input_message = message
         self.temperature = temperature
-        self.repetition_penalty= repetition_penalty
+        self.repetition_penalty = repetition_penalty
         self.top_p = top_p
         self.max_length = max_length
         self.num_beams = num_beams
@@ -66,7 +66,7 @@ class pai_llama:
         ws.run_forever(ping_interval=2)
         return self.received_message.split('<br/>')[1:]
 
-llama2_pai_client = pai_llama(api_key="Y2ZlMmYxMTNmZTUyYjgxZjAyYjgxYTg1ZDBhOTg2MDQyYmJhMjQ5Zg==")
+llama2_pai_client = pai_llama(api_key="MDQzZTdmNTdiY2M5MjYxODg4MjgzZDE3YjgxNWJlMGJlNzE0NjI0NA==")
 res = llama2_pai_client.generate(
     message="tell me about alibaba cloud PAI", 
     temperature=0.95
